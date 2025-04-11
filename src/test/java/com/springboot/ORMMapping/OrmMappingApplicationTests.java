e37bae9794181ae15e4c3a43ae1e4073192283ed
Kesav-Kumar-igo <kesavkumar.b@igosolutions.eu>
2025-04-11 11:11:39 +0530

    initialcommit

---
+++ src/test/java/com/springboot/ORMMapping/OrmMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ORMMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
